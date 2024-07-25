2e0decea818a4e3560db51d1ba566ecccc88c355
vuphuong1794 <phuongvu1794@gmail.com>
2024-07-25 11:45:53 +0700

    updates poll repository, poll services and test in redis insight app

--- server/src/polls/polls.repository.ts
+++ server/src/polls/polls.repository.ts
@@ -7,10 +7,10 @@ import {
 } from '@nestjs/common';
 import { IORedisKey } from 'src/redis.module';
 import { Redis } from 'ioredis';
-import { CreatePollData } from './types';
+import { CreatePollData, AddParticipantData } from './types';
 import { Poll } from 'shared';
 
-@Injectable()
+@Injectable() //cho phép cung cấp kho lưu trữ này dưới dạng dịch vụ hoặc nhà cung cấp cho poll.module
 export class PollsRepository {
   // to use time-to-live from configuration
   private readonly ttl: string;
@@ -18,24 +18,27 @@ export class PollsRepository {
 
   constructor(
     ConfigService: ConfigService,
+    //dung inject để cho phép truy cập tới redisclient
     @Inject(IORedisKey) private readonly redisClient: Redis,
   ) {
     // thoi gian ton tai cua cau hoi
-    this.ttl = ConfigService.get('POLLS_DURATION');
+    this.ttl = ConfigService.get('POLL_DURATION');
   }
 
-  //tao ra poll moi (userID, pollID duoc tao o poll service, con nhung cai con lai lay tu API )
+  //tao ra poll moi (userID, pollID duoc tao o create poll service, con nhung cai con lai lay tu bài viết đầu tiên đến poll controller nên dc lấy từ API)
   async CreatePoll({
     pollID,
     topic,
     votesPerVoter,
     userID,
   }: CreatePollData): Promise<Poll> {
+    //thực hiện lời hứa ở poll-types
     const initialPoll = {
       id: pollID,
       topic,
       votesPerVoter,
       participants: {},
+      //khởi tạo với chưa có người tham gia nào
       adminID: userID,
     };
 
@@ -44,7 +47,7 @@ export class PollsRepository {
         this.ttl
       }`,
     );
-
+    //tạo key để lưu vào redis
     const key = `polls:${pollID}`;
 
     //set du lieu cho key bang redisClient
@@ -54,13 +57,77 @@ export class PollsRepository {
           ['send_command', 'JSON.SET', key, '.', JSON.stringify(initialPoll)],
           ['expire', key, this.ttl],
         ])
-        .exec();
+        .exec(); //thực thi
       return initialPoll;
     } catch (e) {
       this.logger.error(
         `Failed to add poll ${JSON.stringify(initialPoll)}\n${e}`,
       );
       throw new InternalServerErrorException();
+    }
+  }
+
+  async getPoll(pollID: string): Promise<Poll> {
+    this.logger.log(`Attempting to get poll with: ${pollID}`);
+
+    const key = `polls:${pollID}`;
+    try {
+      const currentPoll = await this.redisClient.send_command(
+        'JSON.GET',
+        key,
+        '.',
+      );
+
+      this.logger.verbose(currentPoll);
+
+      // if (currentPoll?.hasStarted) {
+      //   throw new BadRequestException('The poll has already started');
+      // }
+      return JSON.parse(currentPoll);
+    } catch (e) {
+      this.logger.error(`Failed to get pollId ${pollID}`);
+      throw e;
+    }
+  }
+  async AddParticipant({
+    pollID,
+    userID,
+    name,
+  }: AddParticipantData): Promise<Poll> {
+    this.logger.log(
+      `Attempting to add a participant with userID/name: ${userID}/${name} to pollID: ${pollID}`,
+    );
+
+    const key = `polls:${pollID}`;
+    const participantPath = `.participants.${userID}`;
+
+    try {
+      await this.redisClient.send_command(
+        'JSON.SET',
+        key,
+        participantPath,
+        JSON.stringify(name),
+      );
+
+      const pollJSON = await this.redisClient.send_command(
+        'JSON.GET',
+        key,
+        '.',
+      );
+
+      const poll = JSON.parse(pollJSON) as Poll;
+
+      this.logger.debug(
+        `Current Participants for pollID: ${pollID}:`,
+        poll.participants,
+      );
+
+      return poll;
+    } catch (e) {
+      this.logger.error(
+        `Failed to add a participant with userID/name: ${userID}/${name} to pollID: ${pollID}`,
+      );
+      throw e;
     }
   }
 }
--- server/src/polls/polls.service.ts
+++ server/src/polls/polls.service.ts
@@ -1,31 +1,51 @@
+import { PollsRepository } from './polls.repository';
 import { Injectable, Logger } from '@nestjs/common';
 import { CreatePollFields, JoinPollFields, RejoinPollFields } from './types';
 import { createPollID, createUserID } from 'src/ids';
 
 @Injectable()
 export class PollsService {
+  constructor(private readonly PollsRepository: PollsRepository) {}
   private readonly logger = new Logger(PollsService.name);
   async createPoll(fields: CreatePollFields) {
     const pollID = createPollID();
     const userID = createUserID();
 
-    return {
+    const createdPoll = await this.PollsRepository.CreatePoll({
       ...fields,
       userID,
       pollID,
+    });
+    //TODO - create an accessToken based off of pollID and userID
+
+    return {
+      poll: createdPoll,
+      //accessToken
     };
   }
 
   async joinPoll(fields: JoinPollFields) {
     const userID = createUserID();
 
+    this.logger.debug(
+      `Fetching poll with ID: ${fields.pollID} for user with ID: ${userID}`,
+    );
+    const joinedPoll = await this.PollsRepository.getPoll(fields.pollID);
+
+    //TODO - create access Token
+
     return {
-      ...fields,
-      userID,
+      poll: joinedPoll,
+      //accessToken: signedString,
     };
   }
 
   async rejoinPoll(fields: RejoinPollFields) {
-    return fields;
+    this.logger.debug(
+      `Rejoining poll with ID: ${fields.pollID} for user with ID: ${fields.userID} with name: ${fields.name}`,
+    );
+    const joinedPoll = await this.PollsRepository.AddParticipant(fields);
+
+    return joinedPoll;
   }
 }

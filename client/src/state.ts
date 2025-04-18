afcf7d33a9bb6fb7e136c50f7dd9e6506d173b13
vuphuong1794 <phuongvu1794@gmail.com>
2025-04-18 14:18:25 +0700

    add admin and valtio subscribe key

--- client/src/state.ts
+++ client/src/state.ts
@@ -1,5 +1,7 @@
 import { Poll } from "shared/poll-types";
 import { proxy } from "valtio";
+import { derive, subscribeKey } from "valtio/utils";
+import { getTokenPayload } from "./util";
 
 /*
 proxy là một API của Valtio giúp tạo reactive state.
@@ -12,8 +14,14 @@ export enum AppPage {
     WaitingRoom = "WaitingRoom",
 }
 
+export type Me = {
+    id: string;
+    name: string;
+};
+
 export type AppState = {
     isLoading: boolean;
+    me?: Me;
     currentPage: AppPage;
     poll?: Poll;
     accessToken?: string;
@@ -24,6 +32,34 @@ const state:AppState = proxy({
     currentPage: AppPage.Welcome //Ban đầu, currentPage được đặt là AppPage.Welcome
 
 });
+
+const stateWithComputed: AppState = derive(
+    {
+        me: (get) => {
+            const accessToken = get(state).accessToken;
+      
+            if (!accessToken) {
+              return;
+            }
+      
+            const token = getTokenPayload(accessToken);
+      
+            return {
+              id: token.sub,
+              name: token.name,
+            };
+        },
+          isAdmin: (get) => {
+            if (!get(state).me) {
+              return false;
+            }
+            return get(state).me?.id === get(state).poll?.adminID;
+        },
+    },
+    {
+        proxy: state,
+    }
+)
 
 const actions = {
     setPage: (page: AppPage): void => {
@@ -46,4 +82,14 @@ const actions = {
     },
 };
 
-export { state, actions };
+//theo dõi sự thay đổi của accesstoken 
+//nếu có accessToken và poll thì lưu vào localStorage, nếu không thì xóa khỏi localStorage
+subscribeKey(stateWithComputed, 'accessToken', () => {
+    if(state.accessToken && state.poll) {
+        localStorage.setItem('accessToken', state.accessToken);
+    } else {
+        localStorage.removeItem('accessToken');
+    }
+});
+
+export { stateWithComputed as state, actions };
